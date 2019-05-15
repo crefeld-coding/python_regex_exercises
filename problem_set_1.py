4f9d782ed4346a9a04267e19c51fab393d503f36
Filip Furmanek <ffurmanek@crefeld.org>
2019-05-14 22:22:34 -0400

    Initialize with simple unittest test
    As references, I'm using these documents:
    
    - https://github.com/uwhpsc-2016/example-python-homework/
    - https://realpython.com/python-testing/
    - https://docs.python-guide.org/writing/tests/
    - https://docs.python.org/3/library/unittest.html

---
+++ problem_set_1.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class BasicMatching(unittest.TestCase):
+    """Fill in a regular expression that will match the
+    target strings, without matching the other strings.
+    """
+
+    def test_pets(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)  # Use 1 for more compact output.
