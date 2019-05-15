c87e1daff259dd20ecc802684638f5d44811f1f1
Filip Furmanek <ffurmanek@crefeld.org>
2019-05-14 23:54:22 -0400

    Add price mentioning exercise
    Slightly more complicated than finding plain numbers. The cents
    aren't always present. And a dollar sign is a special character
    in regular expressions, so that's another gotcha.

--- problem_set_1.py
+++ problem_set_1.py
@@ -33,6 +33,31 @@ class BasicMatching(unittest.TestCase):
             b = bool(regex.search(text))
             self.assertFalse(b, f"False match: {text}")
 
+    def test_mentions_price(self):
+        has_price = [
+            "Robot kit: $199.99",
+            "Jetpack: $7.89",
+            "Boombox: $11",
+            "CD with Never Gonna Give You Up: $0.50",
+            ]
+        no_price = [
+            "Pranking the meeting on the 11th floor: priceless",
+            "For everything else that costs $$$, there's bitcoins.",
+            "The current temperature is 72.9 degrees centigrade.",
+            "This 98 degrees CD is expen$ive!"
+            ]
+
+        pattern = r""  # TODO
+
+        regex = re.compile(pattern)
+
+        for text in has_price:
+            b = bool(regex.search(text))
+            self.assertTrue(b, f"Failed to match: {text}")
+
+        for text in no_price:
+            b = bool(regex.search(text))
+            self.assertFalse(b, f"False match: {text}")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)  # Use 1 for more compact output.
